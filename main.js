3cd693ec8c594222ee80cb871ab550a897d7425a
njordr <njordrenterprises@gmail.com>
2024-03-24 23:04:47 -0500

    Working with dicitonaries

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 5,
-            onSale: false
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ],
+            sizes: [
+                { id: 5678, size: 'S'},
+                { id: 5679, size: 'M'},
+                { id: 5680, size: 'L'},
+            ]
         }
     }
 })
