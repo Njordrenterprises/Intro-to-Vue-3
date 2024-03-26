f6ff0c68bb4d929ec4180f1ac4955020b7d9d709
njordr <njordrenterprises@gmail.com>
2024-03-25 21:18:27 -0500

    compute functions

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
